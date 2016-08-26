a6d1cb40d2bf2aade2d39b524226c6b7953f3809
Matthew Thompson <matthew.thompson.a@gmail.com>
2016-08-26 15:33:38 -0400

    initial commit of file(s)

---
+++ condition.php
@@ -0,0 +1,15 @@
+<?php
+$a = 5;
+$b = 2;
+
+if ($a == $b) {
+    echo ' values are equal ';
+}
+elseif ($a < $b)
+{ echo "$a is less than $b";}
+elseif ($a > $b)
+{ echo "$a is greater than $b";}
+else {
+  echo 'values are NOT equal';
+}
+?>
